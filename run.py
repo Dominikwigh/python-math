f4467ac72f0596304d82e2495aaacfabf783f755
Dominikwigh <dominik-00@hotmail.com>
2023-01-04 14:37:14 +0000

    add a title function

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random 
+import operator 
+
+
+def display_intro():
+    """
+    display a title for the the game
+    """
+    title = "** A Simple Math Test **"
+    print("*" * len(title))
+    print(title)
+    print("*" * len(title))
+
+display_intro()
