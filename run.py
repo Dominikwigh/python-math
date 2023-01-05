1079fdd0731351f4e5b41a1c36acd0290be033ee
Dominikwigh <dominik-00@hotmail.com>
2023-01-05 09:17:43 +0000

    add a print message with the maths question

--- run.py
+++ run.py
@@ -6,19 +6,22 @@ def display_intro():
     """
     display a title for the the game
     """
-    title = "** A Simple Math Test **"
+    title = "** Welcome To Math Practice! **"
     print("*" * len(title))
     print(title)
     print("*" * len(title))
+
 
 display_intro()
 
 name = input("Enter Yout Name To Start The Game: ")
 print("Hello " + name + "! " + "Good Luck" + "!")
 
+
 def random_question():
     """
-    a dictionary with operators and function to randomly select the number on the right and left 
+    a dictionary with operators and function to randomly
+     select the number on the right and left 
     """
     operators = {
         "+": operator.add,
@@ -31,5 +34,8 @@ def random_question():
     num1 = random.randint(1, 25)
     num2 = random.randint(1, 25)
     operation = random.choice(list(operators.keys()))
+    answer = operators.get(operation)(num1, num2)
+    print(f'What is {num1} {operation} {num2}?')
+    return answer
 
 
